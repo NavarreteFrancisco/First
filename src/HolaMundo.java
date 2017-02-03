95b3991346992a72469daa26d9d0ec0391ab744a
admin <admin@10.0.2.15>
2017-02-03 15:55:25 +0100

    Segundo intento

---
+++ src/HolaMundo.java
@@ -0,0 +1,11 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		String s="HolaMundo";
+		int a=10;
+		System.out.println(s+a);
+	}
+
+}
